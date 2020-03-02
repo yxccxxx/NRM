ebf82980a07fbac564f351d67be190b8d11e9bb2
Yikuan Xia <yix146@ucsd.edu>
2020-03-02 05:14:53 -0800

    assembly

--- float2int.py
+++ float2int.py
@@ -6,19 +6,24 @@ def float2int(floatNum):
     bit2shift = exp - 25
     if bit2shift < 0:
         bit2shift = -bit2shift
-        roundup = 0
-        print("{0:b}".format(mantissa))
-        while bit2shift > 0:
-            if bit2shift > 1:
-                roundup |= (mantissa & 1)
-            else:
-                roundup += (mantissa & 1)
-            mantissa >>= 1
-            bit2shift -= 1
-        if mantissa & 1:
-            roundup += 1
-        if roundup >= 2:
-            mantissa += 1
+        # roundup = 0
+        # while bit2shift > 0:
+        #     if bit2shift > 1:
+        #         roundup |= (mantissa & 1)
+        #     else:
+        #         roundup += (mantissa & 1)
+        #     mantissa >>= 1
+        #     bit2shift -= 1
+        # if mantissa & 1:
+        #     roundup += 1
+        integerPart = mantissa >> bit2shift
+        firstFracBit = mantissa << (16 - bit2shift) >> 15
+        if firstFracBit == 1:
+            if integerPart << 15 >> 15 == 1:
+                integerPart += 1
+            elif mantissa << (16-bit2shift) >> (17-bit2shift) != 0:
+                integerPart += 1
+        return integerPart
     else:
         mantissa <<= bit2shift
     return mantissa
